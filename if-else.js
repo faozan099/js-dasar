cf1672d17e0e5d8c97107e3d5f232ecb69354be8
Muhammad Faozan <fauzanaunillah13@gmail.com>
2023-04-18 10:28:57 +0700

    test

---
+++ if-else.js
@@ -0,0 +1,26 @@
+// if & else ------------------------------------------------------------------------------------------------------
+// ======================== contoh
+let angka = prompt('masukan angka')
+
+if(angka % 2 == 0) {
+    alert(angka + ' bilangan GENAP')
+} else{
+    alert(angka + ' bilangan GANJIL')
+}
+// hasil di tentukan user 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// latihan -------------------------------------------------------------------------------------------------------
+// ======================= jawaban
+let jumlahAngkot = 10;
+let angkotBeroprasi = 6;
+
+for(let noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++) {
+    if(noAngkot <= 6){
+        console.log('angkot no ' + noAngkot + ' angkot sedang beroperasi.');
+    } else{
+        console.log('angkot no ' + noAngkot + ' tidak beroperasi.');
+    }
+}
+// end
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
